2c6ae3b847484bce9af1797c43d5143dbdd029de
Özgür <zgr@DESKTOP-1NLBACI>
2022-12-12 20:33:16 +0300

    Add project files.

---
+++ HackerRank30DaysOfCodeD16/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HackerRank30DaysOfCodeD16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            try
+            {
+                var i = int.Parse(S);
+                Console.WriteLine(i);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+}
